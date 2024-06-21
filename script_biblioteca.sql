a38a0bf121a2fc0af186662ed8fb4d4efab65aba
gelo200815 <160252932+gelo200815@users.noreply.github.com>
2024-06-21 11:59:11 -0300

    Add files via upload

--- script_biblioteca.sql
+++ script_biblioteca.sql
@@ -7,3 +7,14 @@ create table professor(
 	cpf varchar(45),
 	pk int
 );
+
+create table sala(
+	quantidade_aluno int,
+	numero_sala varchar(4),
+    pk int
+);
+
+create table turma(
+	nome_turma varchar(45),
+    pk int
+);
